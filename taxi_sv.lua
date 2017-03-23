9f601cc148bc55c133dcca0168a5856e08875871
Glenn Hermans <glhermans@gmail.com>
2017-03-23 06:14:37 +0100

    Compatibility with essentialmode2.0

--- taxi_sv.lua
+++ taxi_sv.lua
@@ -3,20 +3,9 @@ AddEventHandler('taxi:success', function()
   print("Player ID " ..source)
 	-- Get the players money amount
 TriggerEvent('es:getPlayerFromId', source, function(user)
-  print('Player current cash ' ..user.money)
-  print('Player permission ' ..user.permission_level)
-
-  total = tonumber(user.money) + math.random(100, 500);
-  print('Players new cash amount ' ..total)
+  total = math.random(100, 500);
   -- update player money amount
- TriggerEvent("es:setPlayerData", source, "money", total, function(response, success)
-   print('update Response ' ..response)
-
-   if(success)then
-    print('Success data ' ..success)
-     TriggerClientEvent('es:activateMoney', source, total)
-     TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Your money has been updated to ~g~$".. tonumber(total))
-   end
-  end)
+  user:addMoney((total + 0.0))
+ TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Your money has been updated to ~g~$".. tonumber(total))
  end)
 end)
